40455d1dce8ecc740b3688eeb5e16260dc974056
Eduardo Diez Baez <eduardo.diez.baez@live.com>
2014-08-03 02:21:30 +0200

    Sunday and look working ok
    my project

--- server.R
+++ server.R
@@ -1,16 +1,67 @@
 ######################################
-##### minimal example - server.R #####
+#####     Biorhythm server.R     #####
 ######################################
-library(shiny) # load shiny at beginning at both scripts
-shinyServer(function(input, output) { # server is defined within
-  # these parentheses
-  output$textDisplay <- renderText({ # mark function as reactive
-    # and assign to
-    # output$textDisplay for
-    # passing to ui.R
-    paste0("You said '", input$comment, # from the text
-           "'. There are ", nchar(input$comment), # input control as
-           " characters in this." # defined in ui.R
-    )
+
+# load shiny at beginning at both scripts
+library(shiny)
+
+# and load anything we are going to need
+library(reshape)                        # to melt 
+library(ggplot2)                        # to plot
+library(ggthemes)                       # to give a nice look
+
+# server is defined here
+shinyServer(function(input, output) {         
+  
+  output$thebioplot <- renderPlot({
+    
+    # dates between dates
+    # the time zone should be the corresponded with the aplication server
+    #
+    mydays <- as.numeric(difftime(input$today,  input$birthday, tz="Europe/London"), units="days")
+    
+    # initialize the variables
+    myDate <- rep("", input$amountofdays)
+    Health <- Emotional <- Intelectual <- rep(0, input$amountofdays)
+    
+    # calculate values with the imputs from ui.R
+    for (i in 1 : input$amountofdays) {
+      myDate[i] <- paste("+", as.character(i-2), sep="")
+      Health[i] <- sin(2*pi*(mydays+(i-2))/23)
+      Emotional[i] <- sin(pi*(mydays+(i-2))/14)
+      Intelectual[i] <- sin(2*pi*(mydays+(i-2))/33)
+    }
+    
+    # make a wide table with the values & change the names
+    y <- data.frame (myDate, Emotional, Health, Intelectual)
+    actual <- levels(y$myDate)
+    actual[1] <- "Yesterday"
+    actual[2] <- "Today"
+    actual[3] <- "Tomorrow"
+    levels(y$myDate) <- actual
+    
+    # transform the table from wide to long because ggplot works with long
+    y2 <- melt(y, id=c("myDate"), variable_name="BioCurve")
+    
+    # plot the three values
+    g <- qplot(myDate, value, data=y2,
+               main = paste("Biological Rhythms",
+                            "\n",
+                            "Birthday: ", 
+                            format(input$birthday, "%d- %b -%Y"),
+                            "\n",
+                            "Today: ", 
+                            format(input$today, "%d- %b -%Y"), sep=""),
+               color = BioCurve,
+               group=BioCurve,
+               geom=c("line"))   
+    
+    # make the plot to look better 
+    p <- g +  xlab("") +  ylab("") + theme_wsj() 
+    
+    # show the plot
+    p
+
   })
-})
+  
+  })
--- ui.R
+++ ui.R
@@ -1,22 +1,71 @@
 ###################################
-##### minimal example – ui.R #####
+#####     Biorhythm ui.R      #####
 ###################################
-library(shiny) # load shiny at beginning at both scripts
-shinyUI(pageWithSidebar( # standard shiny layout, controls on the
-  # left, output on the right
-  headerPanel("Minimal example"), # give the interface a title
-  sidebarPanel( # all the UI controls go in here
-    textInput(inputId = "comment", # this is the name of the
-              # variable- this will be
-              # passed to server.R
-              label = "Say something?", # display label for the
-              # variable
-              value = "" # initial value
-    )
+
+# load shiny at beginning at both scripts
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel(paste ("Shiny's Biorhythm")),
+  
+  sidebarPanel(
+    
+    h3("",
+       a("Developing Data Products", href="https://www.coursera.org/course/devdataprod")),
+    
+    img(src = "DevelopingDataProducts.jpg", height = 480, width = 480), 
+    
+    br(), br(),
+    
+    HTML("Peer Assessments project by Eduardo B. Díez"),
+    hr(),
+    h3("",a("Input panel")),
+    dateInput(inputId = "birthday",
+              label = "The day of your birthday (do not lie)",
+              start = "1900-01-01",
+              max = Sys.Date(),
+              value = "1983-09-15" # for some reason the date drop 1 day down
+    ),
+    
+    br(),
+    
+    dateInput(inputId = "today",
+              label = "Today or the interest date",
+              start = "1900-01-01",
+              max = Sys.Date()+100,
+              value = Sys.Date()
+    ),
+    
+    br(),
+    
+    sliderInput(inputId = "amountofdays",
+                label = "Days to show",
+                min = 3,
+                max = 11,
+                value = 9,
+                step = 1)
+    
   ),
-  mainPanel( # all of the output elements go in here
-    h3("This is you saying it"), # title with HTML helper
-    textOutput("textDisplay") # this is the name of the output
-    # element as defined in server.R
+  
+  mainPanel( 
+    
+    HTML('<style type="text/css"> .well { background-color: #F8F2E4; }
+    .shiny-html-output { font-size: 20px; line-height: 21px; } </style>'),
+    h4("A little of history . . . "),
+    h5("The practice of consulting biorhythms was popularized in the 70s through a series of books written by Bernard Gittelson, among which are Biorhythm - A Personal Science (Biorhythm - A Personal Science) Biorhythm Charts of the Famous and Infamous ( Letters biorhythmic famous and infamous) and Biorhythm Sports Forecasting (sports Forecast by biorhythms). Gittelson's company, Biorhytm Computers Inc., earned money by selling biorhythm calculators and personalized letters biorhythmic yet never anything in predicting sports scores." , 
+       a("(Wikipedia)", href="http://es.wikipedia.org/wiki/Biorritmo")),
+    h6("(It should be remembered that this prediction is free. Also, the author is not liable for any collateral damage by misuse from your side. You could read the", 
+       a("  ioslides RStudio interactive style presentation ", href="http://turpial.shinyapps.io/SlidesVerControl/SlidesVerControl.Rmd#1"), "about this application.)"),
+    
+    hr(),
+    
+    textOutput("textDisplay"),
+    plotOutput('thebioplot'),
+    
+    # giving some air at the bottom so the aplication fix inside 
+    # the RStudio slides presentation 
+    hr(), br(), br(), br(), br(), br()
+    
   )
-))
+)
+)
